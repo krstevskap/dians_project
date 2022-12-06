b628a327191a161fc2c8685afe20dcc7d9cbf600
pkrstevska <krstevskap16@gmail.com>
2022-12-06 23:53:10 +0100

    init

--- src/main/java/mk/finki/dians/dians_project/DiansProjectApplication.java
+++ src/main/java/mk/finki/dians/dians_project/DiansProjectApplication.java
@@ -2,8 +2,10 @@ package mk.finki.dians.dians_project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 
+@ServletComponentScan
 @SpringBootApplication
 public class DiansProjectApplication {
 
