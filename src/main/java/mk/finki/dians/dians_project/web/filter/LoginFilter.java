aefe22a2cde7f4c042d8edb04f928bd3aa8416e6
pkrstevska <krstevskap16@gmail.com>
2022-12-11 18:04:33 +0100

    Register change

--- src/main/java/mk/finki/dians/dians_project/web/filter/LoginFilter.java
+++ src/main/java/mk/finki/dians/dians_project/web/filter/LoginFilter.java
@@ -18,18 +18,17 @@ public class LoginFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        HttpServletRequest req=(HttpServletRequest) request;
-        HttpServletResponse resp=(HttpServletResponse) response;
+        HttpServletRequest req = (HttpServletRequest) request;
+        HttpServletResponse resp = (HttpServletResponse) response;
 
-        User user=(User) req.getSession().getAttribute("user");
+        User user = (User) req.getSession().getAttribute("user");
 
-        String path=req.getServletPath();
+        String path = req.getServletPath();
 
-        if(!"/login".equals(path) && !"/register".equals(path) && !"home.css".equals(path)&& !"loginStyle.css".equals(path)&& !"register.css".equals(path) && user==null){
+        if ("/home/maps".equals(path) && user == null) {
             resp.sendRedirect("/login");
-        }
-        else {
-            chain.doFilter(request,response);
+        } else {
+            chain.doFilter(request, response);
         }
     }
 
