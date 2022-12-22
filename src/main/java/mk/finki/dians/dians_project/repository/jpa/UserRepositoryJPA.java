d9d294d1dcb9fd2d63297e46b7f290a2893932b9
Darko <darko_krstevski@icloud.com>
2022-12-22 16:13:27 +0100

    Povrzuvanje na baza na PostgreSQL i postavuvanje vo DockerContainer na istata

---
+++ src/main/java/mk/finki/dians/dians_project/repository/jpa/UserRepositoryJPA.java
@@ -0,0 +1,14 @@
+package mk.finki.dians.dians_project.repository.jpa;
+
+import mk.finki.dians.dians_project.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepositoryJPA extends JpaRepository<User,String> {
+
+    Optional<User> findByUsername(String username);
+}
+
