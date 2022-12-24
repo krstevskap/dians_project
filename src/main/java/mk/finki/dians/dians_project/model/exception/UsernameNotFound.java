16856d4cf4fd4bbdddc532e6c05c6ed8293ea2b7
Darko <darko_krstevski@icloud.com>
2022-12-24 13:03:00 +0100

    Refaktoriranje na exceptions

--- src/main/java/mk/finki/dians/dians_project/model/exception/UsernameNotFound.java
+++
@@ -1,8 +0,0 @@
-package mk.finki.dians.dians_project.model.exception;
-
-public class UsernameNotFound extends RuntimeException{
-
-    public UsernameNotFound(String username) {
-        super(String.format("This %s username already exists",username));
-    }
-}
