0cd3a997cb47bca4af27119760651acb9ebb53a3
Darko <darko_krstevski@icloud.com>
2022-12-24 13:31:41 +0100

    Refaktoriranje na service delot

--- src/main/java/mk/finki/dians/dians_project/service/impl/UserService.java
+++ src/main/java/mk/finki/dians/dians_project/service/impl/UserService.java
@@ -1,22 +1,9 @@
 package mk.finki.dians.dians_project.service.impl;
 
 import mk.finki.dians.dians_project.model.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-
-import java.util.Optional;
-
 public interface UserService  extends UserDetailsService {
-
-
-
-
       User register(String username, String password, String name, String surname,String confirmPassword, String email);
-
       User login(String username, String password);
-
-
-//    void deleteUser(String username);
-
 
 }
